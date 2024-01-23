fb1b09a30229957394f0cbc3c1fe9d4b9df583c5
ganeshvaidkar <vaidkarganesh>
2024-01-23 16:00:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css'
 import Child from "./Child"
-import Demo from "./Demo"
 
 export default function App() {
   return (
     <main>
       <Child />
-      {/* <Demo/> */}
     </main>
   )
 }
